f0c4a7f5476b1834b3e20358c996db788363d788
Brian Lewis <brian@modulesunraveled.com>
2016-09-12 10:39:36 -0500

    Removed unnecessary comment from FourKScripts.sh

--- scripts/fourk/FourKScripts.sh
+++ scripts/fourk/FourKScripts.sh
@@ -8,6 +8,5 @@ fi
 
 if [ -f drush/contrib/drush-shell-aliases/drushrc.php ]
   then
-    #touch drush/contrib/drush-shell-aliases/test.txt
     mv drush/contrib/drush-shell-aliases/drushrc.php drush/drushrc.php
 fi
