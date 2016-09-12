6086a359b7e19044d4b242f294f36ebad2aa45d6
Brian Lewis <brian@modulesunraveled.com>
2016-09-12 10:36:54 -0500

    Required modules-unraveled/drush-shell-aliases and setup FourKScripts.sh to move it into place.

--- scripts/fourk/FourKScripts.sh
+++ scripts/fourk/FourKScripts.sh
@@ -5,3 +5,9 @@ if [ ! -f vendor/pattern-lab/core/config/config.yml ]
   then
     ln -s ../../../../pattern-lab-config.yml ./vendor/pattern-lab/core/config/config.yml
 fi
+
+if [ -f drush/contrib/drush-shell-aliases/drushrc.php ]
+  then
+    #touch drush/contrib/drush-shell-aliases/test.txt
+    mv drush/contrib/drush-shell-aliases/drushrc.php drush/drushrc.php
+fi
