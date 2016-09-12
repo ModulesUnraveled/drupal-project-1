0e6568582627472c5b88bcafb41da8bd292e9227
Brian Lewis <brian@modulesunraveled.com>
2016-09-12 12:44:41 -0500

    Stopped moving the drush-shell-aliases file up to /drush, and instead included it in the root-level drushrc.php.

--- scripts/fourk/FourKScripts.sh
+++ scripts/fourk/FourKScripts.sh
@@ -5,8 +5,3 @@ if [ ! -f vendor/pattern-lab/core/config/config.yml ]
   then
     ln -s ../../../../pattern-lab-config.yml ./vendor/pattern-lab/core/config/config.yml
 fi
-
-if [ -f drush/contrib/drush-shell-aliases/drushrc.php ]
-  then
-    mv drush/contrib/drush-shell-aliases/drushrc.php drush/drushrc.php
-fi
